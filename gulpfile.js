a51be9e10558fcd7177c2f9afe8dc7693ad3b49c
Max Plumer <maxplumer12@gmail.com>
2021-06-04 12:49:24 -0400

    themekit-gulp-scss

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const themeKit = require('@shopify/themekit');
+const sass = require('gulp-sass');
+const clean = require('gulp-clean-css');
+
+gulp.task('sass', function() {
+    return gulp.src('styles/custom-styles.scss')
+    .pipe(sass())
+    .pipe(clean({ compatibility: 'ie11'}))
+    .pipe(gulp.dest('assets'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('styles/**/*.scss', gulp.series('sass'))
+    themeKit.command('watch', {
+        env: 'development',
+    })
+})
